7e186b1db654eaf26e1f897c0be4708da047fdef
mmoraguesr11 <47116337+mmoraguesr11@users.noreply.github.com>
2019-12-16 20:58:19 +0100

    Update index.php

--- web/index.php
+++ web/index.php
@@ -21,12 +21,12 @@ if ($num > 0) { // If it ran OK, display the records.
   while ($row = mysqli_fetch_array($s, MYSQLI_ASSOC)) {
     echo '<div>
       <img src="">
-      <p>' . $row['title'] . '</p>
-      <p>' . $row['genre'] . '</p>
-      <p>' . $row['year'] . '</p>
-      <p>' . $row['director'] . '</p>
-      <p>' . $row['duration'] . '</p>
-      <p>' . $row['session'] . '</p>
+      <h2>Title: ' . $row['title'] . '</h2>
+      <p>Genre: ' . $row['genre'] . '</p>
+      <p>Year: ' . $row['year'] . '</p>
+      <p>Director: ' . $row['director'] . '</p>
+      <p>Duration: ' . $row['duration'] . '</p>
+      <p>Session: ' . $row['session'] . '</p>
     </div>';
   }
 }
