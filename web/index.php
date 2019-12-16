36bb79d8de6009fb1747f0e7ac9b097a356125f4
mmoraguesr11 <47116337+mmoraguesr11@users.noreply.github.com>
2019-12-16 19:57:12 +0100

    Create index.php

---
+++ web/index.php
@@ -0,0 +1,14 @@
+<?php # Script 3.7 - index.php #2
+// This function outputs theoretical HTML
+// for adding ads to a Web page.
+
+$page_title = 'MovieTime!';
+include('includes/header.php');
+?>
+
+<div class="page-header"><h1>Premieres</h1></div>
+
+
+<?php
+include('includes/footer.html');
+?>
