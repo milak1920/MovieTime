633d16ca140e03e4f16a1bc9f7c9fa850eb10f3a
milak1920 <34102395+milak1920@users.noreply.github.com>
2020-01-15 17:24:27 +0100

    Update armchairs.php

--- web/armchairs.php
+++ web/armchairs.php
@@ -2,26 +2,112 @@
 include('includes/header.php');
 ?>
 <h1>Tickets and armchairs</h1>
-
-<form action="purchase.php" method="post">
-  <h2>Select the number of tickets</h2>
-  <select name="tickets">
-    <option value="1">1</option>
-    <option value="2">2</option>
-    <option value="3">3</option>
-    <option value="4">4</option>
-    <option value="5">5</option>
-    <option value="6">6</option>
-    <option value="7">7</option>
-    <option value="8">8</option>
-    <option value="9">9</option>
-    <option value="10">10</option>
-  </select>
-  <h2>Select the armchairs</h2>
+<?php
+ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+	  
+//    if (isset ($_REQUEST['tickets'])){
+//        $tickets=$_REQUEST['tickets'];
+//    } 
+    
+       
   
+//    if ($tickets==count($armchair)){
+//        
+//       echo "<h1>Your tickets</h1>";
+//       	// Table header:
+//	echo '<table width="60%">
+//	<thead>
+//	<tr>
+//		<th align="left"><strong>Delete</strong></th>
+//		<th align="left"><strong>Title</strong></th>
+//		<th align="left"><strong>Genre</strong></th>
+//		<th align="left"><strong>Session</strong></th>
+//    <th align="left"><strong>Number of tickets</strong></th>
+//    <th align="left"><strong>Number of armchairs</strong></th>
+//	</tr>
+//	</thead>
+//	<tbody>
+//	'; 
+//        
+//        	// Fetch and print all the records:
+//	while ($row = mysqli_fetch_array($r, MYSQLI_ASSOC)) {
+//		echo '<tr>
+//			<td align="left"><input type="button" onclick="delete()">Delete</td>
+//			<td align="left">' . $row['title'] . '</td>
+//			<td align="left">' . $row['genre'] . '</td>
+//			<td align="left">' . $row['session'] . '</td>
+//      		<td align="left">'.$_POST['tickets'].'</td>
+//      		<td align="left">'.$_POST['armchair'].'</td>
+//		</tr>
+//		';
+//	}
+//
+//	echo '</tbody></table>';
+//        
+//    }
+     
+
+ }
+        
+     
+?>
+
+
+<form action="armchairs.php" method="post">
+  <h2>Select the number of tickets</h2>
+    <?php
+        $tickets=range(1,20);
+            echo '<select name="tickets">';
+            foreach($tickets as $value){
+                echo '<option value="value">'.$value.'</option>';
+            }
+            echo "</select>";
+    ?>
+  <h2>Select the armchairs</h2>
+  <div id="divid">
+<?php
+      require('../mysqli_connect.php');
+      // Define the query:
+      $m = "SELECT armchair_id, image FROM armchairs";
+      $s = @mysqli_query($dbm, $m);
+      // Count the number of returned rows:
+      $num = mysqli_num_rows($s);
+            if ($num > 0) { // If it ran OK, display the records.
+            while ($row = mysqli_fetch_array($s, MYSQLI_ASSOC)) {
+            echo '<img   onclick="cambia(this.id)"  width="9%" id="'.$row['armchair_id'].'" src="'.$row['image'].'" name="sillas">'; 
+                } 
+            }     
+?>   
+    </div><br><br>
   <input type="submit" value="Send">
 </form>
-
+ 
+<script>
+ var cuantosClicks = 0;
+ function cambia(id){
+    src = "";
+    cuantosClicks++;
+    // En realidad va sin las comillas simples ''
+    if(cuantosClicks % 2 == 0){
+        src= "images/ocupado.jpg";
+    }else{
+        src= 'images/libre.jpg';
+    }
+    $.ajax(
+        {
+            url : "update.php",
+            type: "POST",
+            data : {idP: id,imagen:src}
+        })
+    .done(function(data) {
+        //$("#respuesta").html(data);
+        //alert(id)
+      $("#divid").load(" #divid");
+    });
+ }
+</script>
+ 
+ 
 <?php
 include('includes/footer.html');
 ?>
