d64dca0e818f0ba3d39acc96f0c9c6cc5fef42a3
milak1920 <34102395+milak1920@users.noreply.github.com>
2020-01-15 17:26:00 +0100

    Update header.php

--- web/includes/header.php
+++ web/includes/header.php
@@ -7,7 +7,9 @@
 <title><?php echo $page_title; ?></title>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 <link href="css/sticky-footer-navbar.css" rel="stylesheet">
-    <link href="css/style.css" rel="stylesheet">
+<link href="css/style.css" rel="stylesheet">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
 </head>
 <body>
 <?php
