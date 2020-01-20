a8f2b6ebc65401276d5d39db4b5a7b089b6beaed
milak1920 <mila.karim0034@gmail.com>
2020-01-20 21:59:30 +0100

    Update mysqli_connect.php

--- mysqli_connect.php
+++ mysqli_connect.php
@@ -3,10 +3,10 @@
 // This file also establishes a connection to MySQL,
 // selects the database, and sets the encoding.
 // Set the database access information as constants:
-define('DB_USER', 'root');
-define('DB_PASSWORD', '');
+define('DB_USER', 'id12308554_root');
+define('DB_PASSWORD', '12345');
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'movietime');
+define('DB_NAME', 'id12308554_movietime');
 // Make the connection:
 $dbm = @mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) OR die('Could not connect to MySQL: ' . mysqli_connect_error() );
 // Set the encoding...
