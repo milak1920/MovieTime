bec4e2bb491101b427f7c14571d22fe35a3d5493
mmoraguesr11 <47116337+mmoraguesr11@users.noreply.github.com>
2020-01-20 12:22:50 +0100

    Update armchairs.php

--- web/armchairs.php
+++ web/armchairs.php
@@ -1,39 +1,46 @@
 <?php # armchairs.php #3
 include('includes/header.php');
+
+require('../mysqli_connect.php');
+      // Define the query:
+      $m = "SELECT armchair_id, image FROM armchairs";
+      $s = @mysqli_query($dbm, $m);
 ?>
 <h1>Tickets and armchairs</h1>
  
 <?php
  
 if ($_SERVER["REQUEST_METHOD"]=="POST"){  
-     //creamos la cookie  
-   
+  $errors = []; // Initialize an error array.
+     //Check tickets 
+   $ti = mysqli_real_escape_string($dbm, $_POST['tickets']);
     
-     $errors = []; // Initialize an error array.
-	// Check for a first name:
-	if (empty($_POST['tickets'])) {
-		$errors[] = 'no ingresaste numero de tickets';
+	// Check sillas:
+	if (empty($_POST['sillas'])) {
+		$errors[] = 'No has seleccionado ninguna butaca';
 	} else {
-		$ar = mysqli_real_escape_string($dbm, trim($_POST['tickets']));
+    if ($_POST['tickets']==count($_POST['sillas'])) {
+      $ar = mysqli_real_escape_string($dbm, $_POST['sillas']);
+    } else {
+      $errors[] = 'No coinciden número de tickets y cantidad de butacas seleccionadas.';
+  }
  	} 
- 
-     
      
 	if (empty($errors)) { // If everything's OK.
-		// Register the user in the database...
+		// Register the purchase in the database...
 		// Make the query:
-		$q = "INSERT INTO purchase (movie_id, armchair_id) VALUES ('$mi', '$ar')";
+		$q = "INSERT INTO purchase (tickets, armchair_id) VALUES ('$ti', '$ar')";
 		$r = @mysqli_query($dbm, $q); // Run the query.agarra la query y lo envia a la base de datos 
         
  
 		 
 	} else { // Report the errors.
 		echo '<h1>Error!</h1>
-		<p class="error">The following error(s) occurred:<br>';
+		<p class="error">Ha ocurrido un problema:<br>';
 		foreach ($errors as $msg) { // Print each error.
 			echo " - $msg<br>\n";
 		}
-		echo '</p><p>Please try again.</p><p><br></p>';
+		echo '</p><p>Prueba de nuevo.</p><p><br></p>';
 	} // End of if (empty($errors)) IF.
      
  }
@@ -41,30 +48,27 @@ if ($_SERVER["REQUEST_METHOD"]=="POST"){
  
 
 
-<form action="armchairs.php" method="post">
+<form action="purchase.php" method="post">
   <h2>Select the number of tickets</h2>
     <?php
         $tickets=range(1,20);
             echo '<select name="tickets">';
             foreach($tickets as $value){
-                echo '<option value="value">'.$value.'</option>';
+                echo "<option value=\"$value\">$value</option>\n";
             }
             echo "</select>";
     ?>
   <h2>Select the armchairs</h2>
   <div id="divid">
 <?php
-      require('../mysqli_connect.php');
-      // Define the query:
-      $m = "SELECT armchair_id, image FROM armchairs";
-      $s = @mysqli_query($dbm, $m);
+      
       // Count the number of returned rows:
       $num = mysqli_num_rows($s);
             if ($num > 0) { // If it ran OK, display the records.
             while ($row = mysqli_fetch_array($s, MYSQLI_ASSOC)) {
             echo '<div class="texto"> '.$row['armchair_id'].'º-</div>' ;
 
-            echo '<img   onclick="cambia(this.id)"  width="9%" id="'.$row['armchair_id'].'" src="'.$row['image'].'" name="sillas">'; 
+            echo '<img   onclick="cambia(this.id)"  width="9%" id="'.$row['armchair_id'].'" src="'.$row['image'].'" name="sillas[]">'; 
                 } 
             }     
 ?>   
